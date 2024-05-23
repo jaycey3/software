d7cd6bd0593fe91ac376e0394d6b15925c2d1fbe
Jaycey <jayceybrugge23@gmail.com>
2024-05-23 17:24:05 +0200

    stepservice

--- Semester 2/Web Applicatie/Recipes/Recipes.Logic/Services/StepService.cs
+++ Semester 2/Web Applicatie/Recipes/Recipes.Logic/Services/StepService.cs
@@ -5,6 +5,28 @@ namespace Recipes.Logic.Services
 {
     public class StepService(IStepRepository stepRepository)
     {
+        public List<StepModel>? GetAllSteps(int recipeId)
+        {
+            List<StepModel>? method = stepRepository.GetAllSteps(recipeId);
+            return method;
+        }
 
+        public StepModel? UpdateRecipe(int id, int order, string description)
+        {
+            StepModel? step = stepRepository.UpdateStep(id, order, description);
+            return step;
+        }
+
+        public StepModel? CreateStep(int order, string description, int recipeId)
+        {
+            StepModel? step = stepRepository.CreateStep(order, description, recipeId);
+            return step;
+        }
+
+        public string? DeleteStep(int id)
+        {
+            stepRepository.DeleteStep(id);
+            return null;
+        }
     }
 }
